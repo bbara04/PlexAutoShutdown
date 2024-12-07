0fa070a9c1262a5ad2f56b272d5235a5ec851381
bbara04 <bdbarni@gmail.com>
2024-12-07 15:05:21 +0100

    Remove '.' from end of telegram message

--- shutdown.sh
+++ shutdown.sh
@@ -4,7 +4,7 @@ echo "shutdown" > /home/bbara/PlexAutoShutdown/uptime
 
 TOKEN="7634927453:AAHKziilCwPPLub2iFAUESttmAqqDY4pZPQ"
 CHAT_ID="5302671789"
-MESSAGE="(Manual) Server has been shutdown at $(date +'%H:%M')."
+MESSAGE="(Manual) Server has been shutdown at $(date +'%H:%M')"
 
 curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" -d chat_id=$CHAT_ID -d text="$MESSAGE"
 
