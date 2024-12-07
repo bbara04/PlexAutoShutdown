1106c1700254f2c67af8b8aaa56367a4036f4ff5
bbara04 <bdbarni@gmail.com>
2024-12-07 14:25:50 +0100

    Add telegramm notification to manual suspend and shutdown

--- shutdown.sh
+++ shutdown.sh
@@ -2,4 +2,10 @@
 
 echo "shutdown" > /home/bbara/PlexAutoShutdown/uptime
 
+TOKEN="5499872795:AAGG2XQ-dbjkGZFIDYPdbvvdJa0XN6WYOHo"
+CHAT_ID="5302671789"
+MESSAGE="(Manual) Server has been shutdown at $(date +'%H:%M')."
+
+curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" -d chat_id=$CHAT_ID -d text="$MESSAGE"
+
 sudo shutdown now
--- suspend.sh
+++ suspend.sh
@@ -11,4 +11,10 @@ echo "$formatted_date" > "/home/bbara/PlexAutoShutdown/suspend"
 
 echo "suspend" > "/home/bbara/PlexAutoShutdown/uptime"
 
+TOKEN="5499872795:AAGG2XQ-dbjkGZFIDYPdbvvdJa0XN6WYOHo"
+CHAT_ID="5302671789"
+MESSAGE="(Manual) Server has been suspended at $(date +'%H:%M')."
+
+curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" -d chat_id=$CHAT_ID -d text="$MESSAGE"
+
 sudo rtcwake -u -s 10800 -m mem && /usr/bin/python3 /home/bbara/PlexAutoShutdown/afterSuspend.py
